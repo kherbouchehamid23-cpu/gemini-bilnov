b36a4d3b9e07edbf9282582c20d74f4237310d9b
kherbouchehamid23-cpu <kherbouche.hamid23@gmail.com>
2026-04-28 18:46:12 +0200

    Delete apps/web directory

--- apps/web/src/app/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="fr">
-      <body>{children}</body>
-    </html>
-  );
-}
